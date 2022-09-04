5bcd36c21afd453dd7f6ebf568be52b174970f0d
Ridar Gustia <ridargp@gmail.com>
2022-09-04 19:43:31 +0700

    Pengenalan konsep state management (redux atau context)

--- src/container/pages/Product/CardProduct/CardProduct.jsx
+++ src/container/pages/Product/CardProduct/CardProduct.jsx
@@ -1,32 +1,8 @@
 //TODO LIBRARY
 import React, {Component} from "react";
+import Counter from "./Counter";
 
 class CardProduct extends Component {
-    state = {
-        order: 0
-    }
-
-    handleCounterChange = (value) => {
-        this.props.ubahCounter(value);
-    }
-
-    handlePlus = () => {
-        this.setState({
-            order: this.state.order + 1
-        }, () => {
-            this.handleCounterChange(this.state.order);
-        });
-    }
-
-    handleMinus = () => {
-        if (this.state.order > 0) {
-            this.setState({
-                order: this.state.order - 1
-            }, () => {
-                this.handleCounterChange(this.state.order);
-            })
-        }
-    }
     render(){
         return(
             <div className="kartu">
@@ -35,11 +11,7 @@ class CardProduct extends Component {
                 </div>
                 <div className="prod-title">Minyak Goreng</div>
                 <div className="prod-price">Rp26.000,00</div>
-                <div className="counter">
-                    <button className="minus" onClick={this.handleMinus}>-</button>
-                    <input type="text" value={this.state.order} />
-                    <button className="plus" onClick={this.handlePlus}>+</button>
-                </div>
+                <Counter ubahCounter={(value) => this.props.ubahCounter(value)}/>
             </div>
         )
     }
