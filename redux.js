6659e424d13a9a8082d184088dfd29378ce653c9
Ridar Gustia <ridargp@gmail.com>
2022-09-14 21:40:55 +0700

    Memahami konsep Redux (Dispatch dan Subscription) part 2

--- redux.js
+++ redux.js
@@ -8,13 +8,47 @@ const initialState = {
 
 //TODO REDUCER
 const rootReducer = (state = initialState, action) => {
-    return state;
+    // console.log(action);
+    // if(action.type === 'ADD_AGE'){
+    //     return {
+    //         ...state,
+    //         age: state.age + 1,
+    //     }
+    // }
+    // if(action.type === 'CHANGE_VALUE'){
+    //     return {
+    //         ...state,
+    //         value: state.value + action.newValue
+    //     }
+    // }
+    // return state;
+    //TODO Agar lebih rapi menggunakan switch
+    switch(action.type){
+        case 'ADD_AGE':
+            return {
+                ...state,
+                age: state.age + 1,
+            }
+        case 'CHANGE_VALUE':
+            return {
+                ...state,
+                value: state.value + action.newValue
+            }
+        default:
+            return state;
+    }
 }
 
 //TODO STORE
 const store = createStore(rootReducer);
 console.log(store.getState());
 
-//TODO DISPATCHING ACTION
-
 //TODO SUBSCRIPTION
+store.subscribe(() => {
+    console.log('Store change:', store.getState());
+});
+
+//TODO DISPATCHING ACTION
+store.dispatch({type: 'ADD_AGE'});
+store.dispatch({type: 'CHANGE_VALUE', newValue: 10});
+console.log(store.getState());
